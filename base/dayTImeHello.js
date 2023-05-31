d59f1a304cd26c1e8fd8d0976d57fd6d5109902e
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-05-31 15:46:45 +0300

    taska

---
+++ base/dayTImeHello.js
@@ -0,0 +1,7 @@
+/*Написать функцию, которой передаем имя, и она возраващает приветствие в зависимости от времени суток (Доброе утро\день\вечер\ночи Иван)*/ 
+
+const dayTimeHello = (userName) => {
+    const date = new Date
+    console.log(date)
+}
+dayTimeHello('name')
--- base/firstWordOfSentense.js
+++ base/firstWordOfSentense.js
@@ -2,6 +2,13 @@ let testText = `
 Ранее типы, используемые в параметре типа в defineProps и defineEmits, 
 были ограничены локальными типами и поддерживали только литералы типов и интерфейсы. 
 Это связано с тем, что Vue должен иметь возможность анализировать свойства интерфейса props для генерации соответствующих опций во время выполнения.`
-const firstWords = (text) => {
-
+const firstWords = (testText) => {
+    let text = testText.split('.')
+    let words = []
+    for(let elem = 0; elem < text.length - 1; elem++){
+        words.push(text[elem].match(/[\p{L}\p{Mn}\p{Nd}\p{Pc}']+/u)[0])
+    }
+    console.log(words.join(', '))
 }
+
+firstWords(testText)
